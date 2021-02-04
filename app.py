4be27d1e317d6897289256cebaefd8565d038092
lenker0 <molazarev@gmail.com>
2021-02-05 00:50:22 +0300

    convert mp3 to wav + add rus language. Try, parse.

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask, render_template, request, redirect
 import speech_recognition as sr
+from pydub import AudioSegment
+import os
+
 
 app = Flask(__name__, template_folder='templates')
+app.app_context().push()
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -14,18 +18,25 @@ def index():
             return redirect(request.url)
 
         file = request.files["file"]
+        file_ext = file.filename.split(".")[-1]
+        print(file_ext)
         if file.filename == "":
             return redirect(request.url)
 
         if file:
+            if file_ext == "mp3":
+                p = os.path.abspath(file.filename)
+                audio = AudioSegment.from_mp3(p)
+                audio.export("temp", format="wav")
+                file = "temp"
             recognizer = sr.Recognizer()
             audio_file = sr.AudioFile(file)
             with audio_file as source:
                 data = recognizer.record(source)
-            transcript = recognizer.recognize_google(data, key=None)
+                print("Working on...")
+            try:
+                transcript = recognizer.recognize_google(data, key=None, language='ru-RU')
+            except:
+                print("Sorry... run again...")
 
     return render_template('index.html', transcript=transcript)
-
-
-if __name__ == "__main__":
-    app.run(debug=True, threaded=True)
